2f60d7f2c6a5f935032504f51c2c81ecbf609767
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-09-25 15:29:43 +0200

    Create global-error component

---
+++ app/global-error.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function GlobalError({ error, reset }) {
+    return (
+        <html>
+            <body>
+                <h1>Something went wrong!</h1>
+                <p>{error.message}</p>
+                <button onClick={() => reset()}>Try again</button>
+            </body>
+        </html>
+    );
+}
