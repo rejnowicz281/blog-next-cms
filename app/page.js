5a6d8e16c26a3cd66686d91fe1dd85161f71e440
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-09-25 16:09:51 +0200

    Give unique key prop to post in list in home page

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default async function Home() {
         <div>
             <h1>Posts</h1>
             {posts.map((post) => (
-                <div>
+                <div key={post.id}>
                     <h2>{post.title}</h2>
                     <p>{post.body}</p>
                     <p>{post.createdAt.toString()}</p>
