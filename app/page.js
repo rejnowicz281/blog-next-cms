c66920ebf958455b97316291c0347aa172346b70
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-09-25 13:59:23 +0200

    Force Home component to use dynamic rendering

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import { getPosts } from "@actions/posts";
 import DeletePostButton from "@components/posts/DeletePostButton";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
     const posts = await getPosts();
 
